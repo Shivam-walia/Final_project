a660bd0910cfebbb75fcc50021b9d0ff2e007995
shivam-walia <shivamw65@gmail.com>
2017-07-15 11:55:05 +0530

    models file modified

--- django_project/myapp/models.py
+++ django_project/myapp/models.py
@@ -4,3 +4,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class user(models.Model):
+
+    name= models.CharField(max_length=255)
+    age= models.IntegerField(default=0)
+    phone= models.CharField(max_length=20)
+    created_on= models.DateTimeField(auto_now_add=True)
+    email= models.EmailField(max_length=20)
+
