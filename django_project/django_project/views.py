a1390298268f51afdfd2ab47adea002912e144f7
shivam-walia <shivamw65@gmail.com>
2017-07-21 21:26:02 +0530

    views are added

--- django_project/django_project/views.py
+++ django_project/django_project/views.py
@@ -3,7 +3,9 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from datetime import datetime
+from myapp.forms import signupform
 # Create your views here.
 def signup_view(request):
     today=datetime.now
-    return render(request,'signup.html',{'today':today})
+    signup_form=signupform()
+    return render(request,'signup.html',{'today':today},{'signup_form':signup_form})
