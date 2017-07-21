4d9e55841801a95c4b791507940d097045a35c34
shivam-walia <shivamw65@gmail.com>
2017-07-22 01:11:56 +0530

    views for signup

--- django_project/django_project/views.py
+++ django_project/django_project/views.py
@@ -5,13 +5,11 @@ from django.shortcuts import render
 from datetime import datetime
 from myapp.forms import signupform
 from django.contrib.auth.hashers import make_password
-from myapp.models import usermod
+from myapp.models import usermode
 # Create your views here.
 def signup_view(request):
-
     if request.method=='GET':
-        #Display signup form
-        
+        #Display signup form       
          signup_form=signupform()
          template_name='signup.html'
     elif request.method=='POST':
@@ -20,13 +18,13 @@ def signup_view(request):
         #Validate form data
         if signup_form.is_valid():
             #validation successful
-            usernme=signup_form.cleaned_data['username']
+            username=signup_form.cleaned_data['username']
             name=signup_form.cleaned_data['name']
             email=signup_form.cleaned_data['email']
             password=signup_form.cleaned_data['password']
             #save to db
-            new_user=usermod(name=name,username=username,password=make_password(password),email=email)
+            new_user=usermode(name=name,username=username,password=make_password(password),email=email)
             new_user.save()
             template_name='success.html'
 
-    return render(request, template_name ,{'signup_form':signup_form})
+    return render(request,template_name,{'signup_form':signupform})
