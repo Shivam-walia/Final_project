d6fc9affac876a03ec66863867d59fd8799e8332
shivam-walia <shivamw65@gmail.com>
2017-07-28 22:50:43 +0530

    views file with login and signup session and cookie

--- django_project/django_project/views.py
+++ django_project/django_project/views.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from datetime import datetime
 from myapp.forms import signupform
 from myapp.forms import loginform
 from django.contrib.auth.hashers import make_password,check_password
-from myapp.models import usermode
+from myapp.models import usermode,sessiontoken
 # Create your views here.
 def signup_view(request):
     if request.method=='GET':
@@ -26,17 +26,20 @@ def signup_view(request):
             #save to db
             new_user=usermode(name=name,username=username,password=make_password(password),email=email)
             new_user.save()
-            template_name='success.html'
+            return redirect("/login/")
+
+          #  template_name='success.html'
 
     return render(request,template_name,{'signup_form':signupform})
 
 
 #login form
 def login_view(request):
+    response_data={}
     if request.method=="GET":
         #display login form
         login_form=loginform()
-        template_name='login.html'
+        #template_name='login.html'
     
     elif request.method=="POST":
         login_form=loginform(request.POST)
@@ -50,17 +53,40 @@ def login_view(request):
                 #password compare
                 if check_password(password,user.password):
                     #login sucessfully
-                    template_name='login_success.html'
+                    new_token=sessiontoken(user=user)
+                    new_token.create_token()
+                    new_token.save()
+                    response=redirect('/feed/')
+                    response.set_cookie(key='session_token',value=new_token.session_token)
+                    return response
+                    
+                   # template_name='login_success.html'
                 else:
                     #Does not login
-                    template_name='login_fail.html'
-            else:
+                #    template_name='login_fail.html'
+                    response_data['message'] = 'Incorrect password  ..! Please try again...'
+        
+    response_data['login_form']=loginform
+
+    return render(request,'login.html',response_data)
+
+    #        else:
                 #user do not exists
-                template_name='login_fail.html'
-        else:
-            print"validation failed"
-            template_name='login_fail.html'
+     #           template_name='login_fail.html'
+      #  else:
+       #     print"validation failed"
+        #    template_name='login_fail.html'
 
         
-    return render(request,template_name,{'login_form':login_form})
+    #return render(request,template_name,{'login_form':login_form})
+
+def check_validation(request):
+    if request.COOKIES.get('session_token'):
+        session=sessiontoken.objects.filter(session_token=request.COOKIES.get('session_token')).first()
+        if session:
+            return session_user
+        else:
+            return None
+
+
 
