fa1118809e723ee37dd4be6cd7c66e099b80525a
shivam-walia <shivamw65@gmail.com>
2017-07-28 22:52:11 +0530

    myapp with session migrations applied

--- django_project/myapp/models.py
+++ django_project/myapp/models.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals
 
 from django.db import models
+import uuid
+
 
 # Create your models here.
 class usermode(models.Model):
@@ -13,3 +15,14 @@ class usermode(models.Model):
     updated_on=models.DateTimeField(auto_now=True)
     email= models.EmailField(max_length=20)
     password=models.CharField(max_length=30)
+
+
+class sessiontoken(models.Model):
+    user=models.ForeignKey(usermode)
+    session_token=models.CharField(max_length=255)
+    created_on=models.DateTimeField(auto_now_add=True)
+    is_valid=models.BooleanField(default=True)
+    def create_token(self):
+        self.session_token=uuid.uuid4()
+
+
