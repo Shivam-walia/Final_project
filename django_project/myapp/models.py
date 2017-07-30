61cc70d001dd9594f0c43fa90b6584d3b59b31a7
shivam-walia <shivamw65@gmail.com>
2017-07-30 18:43:17 +0530

    migrations applied and templates modified with like post comment

--- django_project/myapp/models.py
+++ django_project/myapp/models.py
@@ -24,5 +24,40 @@ class sessiontoken(models.Model):
     is_valid=models.BooleanField(default=True)
     def create_token(self):
         self.session_token=uuid.uuid4()
+#post model use to post
+class PostModel(models.Model):
+    user = models.ForeignKey(usermode)
+    image = models.FileField(upload_to='user_images')
+    image_url = models.CharField(max_length=255)
+    caption = models.CharField(max_length=240)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    has_liked=False
 
+    @property
+    def like_count(self):
+        return len(LikeModel.objects.filter(post=self))
+    #@property
+  #  def comments(self):
+   #     return CommentModel.objects.filter(post=self).order_by('created_on')
+
+#like model to like the posts of the user 
+class LikeModel(models.Model):
+    user = models.ForeignKey(usermode)
+    post = models.ForeignKey(PostModel)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+#comments on user posts
+class CommentModel(models.Model):
+    user = models.ForeignKey(usermode)
+    post = models.ForeignKey(PostModel)
+    comment_text = models.CharField(max_length=555)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    upvoted = False
+
+    @property
+    def upvote(self):
+        return len(LikeComm.objects.filter(comment=self))
 
