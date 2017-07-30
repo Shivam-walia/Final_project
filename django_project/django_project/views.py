c46ea2eb77ff4f469ebcedd5f0f7a55d9173bf1b
shivam-walia <shivamw65@gmail.com>
2017-07-30 18:44:20 +0530

    views created for like and comment

--- django_project/django_project/views.py
+++ django_project/django_project/views.py
@@ -4,9 +4,13 @@ from __future__ import unicode_literals
 from django.shortcuts import render,redirect
 from datetime import datetime
 from myapp.forms import signupform
-from myapp.forms import loginform
+from myapp.forms import loginform,PostForm,LikeForm,CommentForm
+from imgurpython import ImgurClient
+     
 from django.contrib.auth.hashers import make_password,check_password
-from myapp.models import usermode,sessiontoken
+from myapp.models import usermode,sessiontoken,PostModel,LikeModel,CommentModel
+from django_project.settings import BASE_DIR
+import os
 # Create your views here.
 def signup_view(request):
     if request.method=='GET':
@@ -69,7 +73,6 @@ def login_view(request):
     response_data['login_form']=loginform
 
     return render(request,'login.html',response_data)
-
     #        else:
                 #user do not exists
      #           template_name='login_fail.html'
@@ -80,11 +83,107 @@ def login_view(request):
         
     #return render(request,template_name,{'login_form':login_form})
 
+def feed_view(request):
+
+    user = check_validation(request)
+    if user:
+        posts = PostModel.objects.all().order_by('created_on')
+        for post in posts:
+            existing_like = LikeModel.objects.filter(post_id=post.id, user=user).first()
+            if existing_like:
+
+                post.has_liked = True
+        return render(request,'feed.html', { 'posts' : posts})
+    else:
+        return redirect('/login/')
+
+def post_view(request):
+    user = check_validation(request)
+    form = PostForm()
+    if user:
+        if request.method == 'GET':
+            form = PostForm()
+            return render(request,'post.html',{'form': form})
+        elif request.method == 'POST':
+            form = PostForm(request.POST,request.FILES)
+            if form.is_valid():
+                image = form.cleaned_data.get('image')
+                caption = form.cleaned_data.get('caption')
+                userpost = PostModel(user=user, image=image,caption=caption)
+                userpost.save()
+                print userpost.image.url
+                path = os.path.join(BASE_DIR , userpost.image.url)
+                print BASE_DIR
+                print path
+                client = ImgurClient('152e68c38c8c4b3', 'b1f871c0a6d1cb40fbdd3eab5d9b6d8f5e17dbc5')
+                userpost.image_url = client.upload_from_path(path,anon=True)['link']
+                userpost.save()
+                return redirect('/feed/')
+            else:
+                form = PostForm()
+                return render(request, 'post.html',{'form': form})
+    else:
+        return redirect('/login/')
+
+
+def like_view(request):
+    user = check_validation(request)
+    if user and request.method == 'POST':
+        form = LikeForm(request.POST)
+        if form.is_valid():
+            post_id = form.cleaned_data.get('post').id
+
+            existing_like = LikeModel.objects.filter(post_id=post_id,user=user).first()
+           
+            if not existing_like:
+                LikeModel.objects.create(post_id=post_id, user=user)
+                poster = PostModel.objects.filter(id=post_id).first()
+                subject = "Your photo was liked"
+                message = "Your photo was liked by" + " " +user.username
+                from_email = EMAIL_HOST_USER
+                to_email = [poster.user.email]
+                send_mail(subject, message, from_email, to_email)
+            else:
+                existing_like.delete()
+                poster = PostModel.objects.filter(id=post_id).first()
+                subject = "Your photo was unliked"
+                message = "Your photo was unliked by" + " " + user.username
+                from_email = EMAIL_HOST_USER
+                to_email = [poster.user.email]
+                send_mail(subject, message, from_email, to_email)
+            return redirect('/feed/')
+
+    else:
+        return redirect('/login/')
+
+#comments in users post
+def comment_view(request):
+    user = check_validation(request)
+    if user and request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            post_id = form.cleaned_data.get('post').id
+            comment_text = form.cleaned_data.get('comment_text')
+            comment = CommentModel.objects.create(user=user,post_id=post_id,comment_text=comment_text)
+            comment.save()
+            poster = PostModel.objects.filter(id=post_id).first()
+            subject = "Comment on your photo"
+            message = str(user.username) + " " + "commented on your photo" + " " + comment_text
+            from_email = EMAIL_HOST_USER
+            to_email = [poster.user.email]
+            send_mail(subject, message, from_email, to_email)
+            return redirect('/feed/')
+        else:
+            return redirect('/feed/')
+    else:
+        return redirect('/login/')
+
+
 def check_validation(request):
     if request.COOKIES.get('session_token'):
         session=sessiontoken.objects.filter(session_token=request.COOKIES.get('session_token')).first()
         if session:
-            return session_user
+            return session.user
         else:
             return None
 
